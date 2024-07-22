76bf5b629917d67ecaef59a83e349c9ddbfb316c
Bryan Borgeson <bryborge@gmail.com>
2024-07-22 00:21:48 -0700

    feature: hot reload

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require 'sinatra/json'
+require 'sinatra/reloader'
 
 # Base controller.
 class BaseApiController < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader # hot-reload in development
+  end
+
   before { content_type :json }
 
   get '/' do
